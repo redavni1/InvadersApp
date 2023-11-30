51230222120d3d53ec18d390e5b579c2ce0f5e50
sseeoo81 <ing20184@gmail.com>
2023-11-30 22:42:55 +0900

    feat: add shooting button on InGame

--- App/app/src/main/java/com/invaders/invadersapp/InGame.java
+++ App/app/src/main/java/com/invaders/invadersapp/InGame.java
@@ -1,5 +1,6 @@
 package com.invaders.invadersapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class InGame extends AppCompatActivity {
     /** ImageView of ship */
     private ImageView ship;
+    private TextView shootBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +25,12 @@ public class InGame extends AppCompatActivity {
         ImageView left_icon = (ImageView) findViewById(R.id.left_icon);
         ImageView right_icon = (ImageView) findViewById(R.id.right_icon);
         ship = (ImageView) findViewById(R.id.ship);
+        shootBtn = (TextView) findViewById(R.id.shoot);
         // set img on left and right buttons
         left_icon.setImageResource(R.drawable.left_button);
         right_icon.setImageResource(R.drawable.right_button);
+
+        shootBtn.setTextColor(Color.WHITE);
 
         left_icon.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -59,6 +65,29 @@ public class InGame extends AppCompatActivity {
                 return true;
             }
         });
+
+//        shootBtn.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                    Log.i("Touch", "down");
+//                    shootBtn.setTextColor(Color.GREEN);
+//                }
+//                if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    Log.i("Touch", "up");
+//                    handlerShooting.post(shootingCooldown);
+//                }
+//                return true;
+//            }
+//        });
+
+        shootBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Touch", "down");
+                handlerShooting.post(shootingCooldown);
+            }
+        });
     }
     /** Handler for left button to control runnableLeft */
     private Handler handlerLeft = new Handler(Looper.getMainLooper());
@@ -79,8 +108,15 @@ public class InGame extends AppCompatActivity {
         public void run() {
             if (ship.getX()+8 < 1008) ship.setX(ship.getX()+8);
             else ship.setX(1008);
-            Log.i("Position", ship.getX()+"");
             handlerRight.postDelayed(this, 17); // fps = 1000/17
+        }
+    };
+
+    private Handler handlerShooting = new Handler(Looper.getMainLooper());
+    private Runnable shootingCooldown = new Runnable() {
+        @Override
+        public void run() {
+
         }
     };
 }
