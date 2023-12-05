6dc759b0745c84a2a73f7cd4eb92d5a476007795
woosuhan <w.oosh522@gmail.com>
2023-12-05 23:03:20 +0900

    feat: item purchase functionality in ItemStore activity

--- App/app/src/main/java/com/invaders/invadersapp/ItemStore.java
+++ App/app/src/main/java/com/invaders/invadersapp/ItemStore.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -21,7 +22,9 @@ public class ItemStore extends AppCompatActivity {
     /* array for item quantity (the number of items this user owns currently) */
     int[] itemQuantity = {0,1,2,3};
     /* the number of coins this user owns currently */
-    int coinQuantity = 0;
+    int coinQuantity = 200 ;
+    /** Adapter for the grid view */
+    ItemAdapter itemAdapter;
 
 
     @Override
@@ -33,15 +36,42 @@ public class ItemStore extends AppCompatActivity {
         gridView = findViewById(R.id.itemGridView);
 
         /* fill the empty grid with items */
-        ItemAdapter itemAdapter = new ItemAdapter(ItemStore.this, itemImage, itemName, itemPrice, itemQuantity);
+        itemAdapter = new ItemAdapter(ItemStore.this, itemImage, itemName, itemPrice, itemQuantity);
         gridView.setAdapter(itemAdapter);
 
-
+        /** Set up item click listener */
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                /* event when user click the item in grid */
+                /** event when user click the item in grid */
+                int price = itemPrice[position];
+
+                if(coinQuantity >= price){
+                    // Deduct coins
+                    coinQuantity -= price;
+                    // Increment item quantity
+                    itemQuantity[position]++;
+                    // Update UI (coin and item quantity)
+                    update_UI();
+                    // Display purchase completion message
+                    Toast.makeText(ItemStore.this, "Purchase complete!",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    // Display not enough coins message
+                    Toast.makeText(ItemStore.this, "Not enough coins!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+    /**
+     * Updates the UI to reflect changes in item quantities and coin balance.
+     */
+    private void update_UI() {
+        // Reflect changes in the adapter
+        itemAdapter.notifyDataSetChanged();
+
+        // Update the coin quantity TextView
+        TextView coinTextView = findViewById(R.id.item_coin);
+        coinTextView.setText((String.valueOf(coinQuantity)));
     }
 }
