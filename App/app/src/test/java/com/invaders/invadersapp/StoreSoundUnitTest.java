2ac56557a29afe83feb3c3f0f05cfe38b529fbc9
daxnii <stgyals3135@hanyang.ac.kr>
2023-12-07 14:00:46 +0900

    feat:add JUnit storesound test

--- App/app/src/test/java/com/invaders/invadersapp/StoreSoundUnitTest.java
+++ App/app/src/test/java/com/invaders/invadersapp/StoreSoundUnitTest.java
@@ -1,4 +1,52 @@
 package com.invaders.invadersapp;
-public class StoreSoundUnitTest {
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import com.invaders.invadersapp.R;
+import com.invaders.invadersapp.sound.StoreBGM;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+
+
+public class StoreSoundUnitTest {
+    @Mock
+    Context mockContext;
+
+    @Mock
+    MediaPlayer mockMediaPlayer;
+
+    @InjectMocks
+    private StoreBGM storebgmMock;
+
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+        when(mockContext.getResources()).thenReturn(mockContext.getResources()); // Mock resources if needed
+        when(mockMediaPlayer.isPlaying()).thenReturn(false); // Set initial state to not playing
+        when(mockMediaPlayer.create(any(), anyInt())).thenReturn(mockMediaPlayer); // Mock MediaPlayer creation
+    }
+
+    public void tearDown(){
+        storebgmMock.stop();
+    }
+
+    public void Test_startmethod(){
+        storebgmMock.start();
+        verify(mockMediaPlayer).start();
+    }
+
+    public void Test_stopmethod(){
+        storebgmMock.stop();
+        verify(mockMediaPlayer).stop();
+        verify(mockMediaPlayer).release();
+    }
 }
+
