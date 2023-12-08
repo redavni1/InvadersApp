94b6ce58745515b9bfb10a65029bec5add67ae38
daxnii <stgyals3135@hanyang.ac.kr>
2023-12-09 01:37:54 +0900

    feat:add javadoc comment

--- App/app/src/androidTest/java/com/invaders/invadersapp/testStorebgm.java
+++ App/app/src/androidTest/java/com/invaders/invadersapp/testStorebgm.java
@@ -14,32 +14,51 @@ import static org.junit.Assert.*;
 
 import com.invaders.invadersapp.sound.StoreBGM;
 
+/**
+ * Test Code: testStorebgm
+ * This code tests the start() method of the StoreBGM class.
+ */
 @RunWith(AndroidJUnit4.class)
 public class testStorebgm {
 
+    /** The target StoreBGM object for testing. */
     private StoreBGM storeBGM;
 
+    /**
+     * Method called before each test method execution.
+     * Initializes the StoreBGM object.
+     */
     @Before
     public void setUp() {
+        // Create a StoreBGM object using the application's Context
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         storeBGM = StoreBGM.getInstance(context);
     }
 
+    /**
+     * Method called after each test method execution.
+     * Releases MediaPlayer resources.
+     */
     @After
     public void tearDown() {
-        // Release any resources used by MediaPlayer
+        // Release MediaPlayer resources
         storeBGM.storebgm_player.release();
     }
 
+    /**
+     * Test method for the start() method of the StoreBGM class.
+     * Checks if MediaPlayer is not playing initially, starts it using the start() method,
+     * and then checks if it is playing after the start.
+     */
     @Test
     public void testStart() {
-        // Check if the MediaPlayer is not playing initially
+        // Check if MediaPlayer is not playing initially
         assertFalse(storeBGM.storebgm_player.isPlaying());
 
-        // Start playing
+        // Start playing using the start() method
         storeBGM.start();
 
-        // Check if the MediaPlayer is playing after start
+        // Check if MediaPlayer is playing after the start
         assertTrue(storeBGM.storebgm_player.isPlaying());
     }
 }
--- App/app/src/main/java/com/invaders/invadersapp/sound/StoreBGM.java
+++ App/app/src/main/java/com/invaders/invadersapp/sound/StoreBGM.java
@@ -5,17 +5,35 @@ import android.media.MediaPlayer;
 
 import com.invaders.invadersapp.R;
 
+/**
+ * This class represents the background music player for the store in the Invaders app.
+ * It follows the singleton pattern to ensure only one instance exists.
+ */
 public class StoreBGM {
+    /** The single instance of the StoreBGM class. */
     public static StoreBGM storebgm;
+
+    /** The MediaPlayer responsible for playing the store background music. */
     public MediaPlayer storebgm_player;
 
-    // Private constructor to prevent direct instantiation
-    public StoreBGM(MediaPlayer player) {
+    /**
+     * Private constructor to prevent direct instantiation.
+     * Initializes the MediaPlayer and sets it to looping mode.
+     *
+     * @param player The MediaPlayer instance for background music.
+     */
+    private StoreBGM(MediaPlayer player) {
         this.storebgm_player = player;
         storebgm_player.setLooping(true);
     }
 
-    // Singleton pattern to ensure only one instance exists
+    /**
+     * Retrieves the singleton instance of the StoreBGM class.
+     * Creates a new instance if it does not exist yet.
+     *
+     * @param context The context used to create the MediaPlayer.
+     * @return The StoreBGM instance.
+     */
     public static StoreBGM getInstance(Context context) {
         if (storebgm == null) {
             MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.store_bgm);
@@ -24,7 +42,9 @@ public class StoreBGM {
         return storebgm;
     }
 
-    // Start playing the background music
+    /**
+     * Starts playing the background music if it is not already playing.
+     */
     public void start() {
         if (!storebgm_player.isPlaying()) {
             storebgm_player.start();
